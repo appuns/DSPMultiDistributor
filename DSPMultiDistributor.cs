b5107eb73d9e182e97f1c8cb1d69cf5f7c8a4f61
Appun <cozy2kozy@gmail.com>
2024-02-18 14:50:28 +0900

    0.0.1

---
+++ DSPMultiDistributor.cs
@@ -0,0 +1,209 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+
+
+namespace DSPMultiDistributor
+{
+    [BepInPlugin("Appun.DSP.plugin.MultiDistributor", "DSPMultiDistributor", "0.0.1")]
+    [HarmonyPatch]
+	public class Main : BaseUnityPlugin
+	{
+
+        //public static float magnificationRate = 0.8f;
+
+        public void Awake()
+        {
+            LogManager.Logger = Logger;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+		}
+
+        //[HarmonyPrefix, HarmonyPatch(typeof(UIDispenserWindow), "_OnClose")]
+        public static void UIDispenserWindow_OnClose_Prefix(UIDispenserWindow __instance)
+        {
+            //__instance.transport.RefreshDispenserTraffic();
+            PlanetTransport planetTransport = __instance.transport;
+
+
+            for (int l = 1; l < planetTransport.dispenserCursor; l++)
+            {
+                LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 1");
+                DispenserComponent dispenserComponent2 = planetTransport.dispenserPool[l];
+                if (dispenserComponent2 != null && dispenserComponent2.id == l)
+                {
+                    LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 2");
+                    int filter = dispenserComponent2.filter;
+                    if (filter == 0)
+                    {
+                        LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 3");
+
+                        if (dispenserComponent2.storageMode == EStorageDeliveryMode.Supply)
+                        {
+                            LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 4");
+                            for (int p = 0; p < dispenserComponent2.storage.size; p++)
+                            {
+                                LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 5");
+                                //int filter2 = dispenserComponent2.storage.grids[p].filter;
+                                int itemId = dispenserComponent2.storage.grids[p].itemId;
+                                if (itemId > 0)
+                                {
+                                    for (int m = 1; m < planetTransport.dispenserCursor; m++)
+                                    {
+                                        DispenserComponent dispenserComponent3 = planetTransport.dispenserPool[m];
+                                        if (dispenserComponent3 != null && dispenserComponent3.id == m && dispenserComponent3.storageMode == EStorageDeliveryMode.Demand && dispenserComponent3.filter == itemId)
+                                        {
+                                            dispenserComponent2.AddPair(l, 0, m, 0);
+                                            dispenserComponent3.AddPair(l, 0, m, 0);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else if (dispenserComponent2.storageMode == EStorageDeliveryMode.Demand)
+                        {
+                            LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 6");
+                            for (int p = 0; p < dispenserComponent2.storage.size; p++)
+                            {
+                                LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 7");
+                                int filter2 = dispenserComponent2.storage.grids[p].filter;
+                                if (filter2 > 0)
+                                {
+                                    LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 8");
+                                    for (int n = 1; n < planetTransport.dispenserCursor; n++)
+                                    {
+                                        LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 9");
+                                        DispenserComponent dispenserComponent4 = planetTransport.dispenserPool[n];
+                                        if (dispenserComponent4 != null && dispenserComponent4.id == n && dispenserComponent4.storageMode == EStorageDeliveryMode.Supply && dispenserComponent4.filter == filter2)
+                                        {
+                                            dispenserComponent2.AddPair(n, 0, l, 0);
+                                            dispenserComponent4.AddPair(n, 0, l, 0);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        int logisticCourierCarries = GameMain.history.logisticCourierCarries;
+                        dispenserComponent2.OnRematchPairs(__instance.factory, planetTransport.dispenserPool, 0, logisticCourierCarries);
+                    }
+                }
+
+            }
+        }
+
+
+        [HarmonyPrefix, HarmonyPatch(typeof(UIDispenserWindow), "_OnClose")]
+        public static void UIDispenserWindow_OnClose_Prefix2(UIDispenserWindow __instance)
+        {
+            GameMain.data.localLoadedPlanetFactory.transport.RefreshDispenserTraffic();
+
+
+         
+
+
+
+
+
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlanetTransport), "RefreshDispenserTraffic")]
+        public static void PlanetTransport_RefreshDispenserTraffic_Postfix(PlanetTransport __instance, int keyId)
+        {
+            for (int l = 1; l < __instance.dispenserCursor; l++)
+            {
+                LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 1");
+                DispenserComponent dispenserComponent2 = __instance.dispenserPool[l];
+                    if (dispenserComponent2 != null && dispenserComponent2.storage != null && dispenserComponent2.id == l)
+                    {
+                        LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 2");
+                        int filter = dispenserComponent2.filter;
+                        if (filter == 0)
+                        {
+                            LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 3");
+
+                            if (dispenserComponent2.storageMode == EStorageDeliveryMode.Supply)
+                            {
+                                LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 4");
+                                for (int p = 0; p < dispenserComponent2.storage.size; p++)
+                                {
+                                    //int itemId = dispenserComponent2.storage.grids[p].itemId;
+                                    LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 5");
+                                    int filter2 = dispenserComponent2.storage.grids[p].filter;
+                                    if (filter2 > 0)
+                                    //     if (itemId > 0)
+                                    {
+                                        for (int m = 1; m < __instance.dispenserCursor; m++)
+                                        {
+                                            LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 6");
+                                            DispenserComponent dispenserComponent3 = __instance.dispenserPool[m];
+                                            if (dispenserComponent3 != null && dispenserComponent3.id == m && dispenserComponent3.storageMode == EStorageDeliveryMode.Demand && dispenserComponent3.filter == filter2)
+                                            {
+                                                dispenserComponent2.AddPair(l, 0, m, 0);
+                                                dispenserComponent3.AddPair(l, 0, m, 0);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            else if (dispenserComponent2.storageMode == EStorageDeliveryMode.Demand)
+                            {
+                                LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 7 " + dispenserComponent2.storage.size);
+                                for (int p = 0; p < dispenserComponent2.storage.size; p++)
+                                {
+                                    LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 8");
+                                    int filter2 = dispenserComponent2.storage.grids[p].filter;
+                                    if (filter2 > 0)
+                                    {
+                                        for (int n = 1; n < __instance.dispenserCursor; n++)
+                                        {
+                                            LogManager.Logger.LogInfo("(----------------------------UIDispenserWindow 9");
+                                            DispenserComponent dispenserComponent4 = __instance.dispenserPool[n];
+                                            if (dispenserComponent4 != null && dispenserComponent4.id == n && dispenserComponent4.storageMode == EStorageDeliveryMode.Supply && dispenserComponent4.filter == filter2)
+                                            {
+                                                dispenserComponent2.AddPair(n, 0, l, 0);
+                                                dispenserComponent4.AddPair(n, 0, l, 0);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            int logisticCourierCarries = GameMain.history.logisticCourierCarries;
+                            dispenserComponent2.OnRematchPairs(__instance.factory, __instance.dispenserPool, keyId, logisticCourierCarries);
+                        }
+                    }
+
+
+
+            }
+        }
+
+        public class LogManager
+        {
+            public static ManualLogSource Logger;
+        }
+    }
+}
---
+++ DSPMultiDistributor/README.md
@@ -0,0 +1,37 @@
+# DSP MultiDistributor
+A distributor can now transport with multiple distributors.<br>
+This don't affect achievements or milestones. <br>
+<br>
+物流配送器が複数の物流配送器と輸送できるようになります。<br>
+実績やマイルストーンには影響しません。<br>
+
+## Features　特徴
+If the items handled by the distributor are not set, the distributor will transport with multiple distributors.Both request and supply are possible.<br>
+Targets items with filters set in the grid in the storage attached by the distributor. Currently only the top storage is targeted.<br>
+When you close the distributor window, connection information with other distributors is updated and distribution begins.<br>
+Simply setting the storage filter does not update the connection information.<br>
+<br>
+I have just created it, so I will make many improvements in the future.Please let me know if you have any ideas.<br>
+<br>
+物流配送器で取り扱うアイテムを設定していない場合、物流配送器は複数の物流配送器と搬送します。要求、供給ともに可能です。<br>
+物流配送器が接続したストレージ内のグリッドにフィルターを設定したアイテムを対象とします。現在一番上のストレージのみが対象となります。<br>
+物流配送器ウインドウを閉じたときに、他の物流配送器と接続情報が更新され、搬送が開始されます。<br>
+ストレージのフィルターを設定しただけでは、接続情報が更新されません。<br>
+<br>
+まだ作ったばかりなので、今後いろいろ改良します。アイデアがありましたらお知らせください。<br>
+
+
+## How to install　インストール方法
+1. Install BepInEx<br>
+2. Drag DSPMultiDistributor.dll into steamapps/common/Dyson Sphere Program/BepInEx/plugins<br>
+<br>
+1. BepInExをインストールします。<br>
+2. DSPMultiDistributor.dllをsteamapps/common/Dyson Sphere Program/BepInEx/pluginsに配置します。<br>
+<br>
+## Contact 問い合わせ先
+If you have any problems or suggestions, please contact DISCORD MSP Modding server **Appun#8284**.<br>
+不具合、改善案などありましたら、DISCORD「DysonSphereProgram_Jp」サーバー**Appun#8284**までお願いします。<br>
+<br>
+## Change Log　更新履歴
+### v0.0.1
+- released, Supported the game version 0.10.28.21308. リリースしました。ゲームバージョン0.10.29.21950に対応しています。
